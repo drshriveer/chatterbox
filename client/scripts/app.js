aa273c7882e0d566c97a23d93006789543e9e657
Hack Reactor Students <github@hackreactor.com>
2013-10-07 16:27:26 -0700

    elements to facilitate chatting in index, methods written to help with chatting

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,46 @@
 // YOUR CODE HERE:
 
-// displayMessage(messages)
+var urlz = 'https://api.parse.com/1/classes/chatterbox';
 
+// displayMessage(messages)
+var displayMessages = function(messageList){
+
+};
+
+var retrieveMessages = function(){
+  $.ajax({
+    url: urlz,
+    type: 'GET',
+    sucess: function(messages){
+      console.log(messages);
+    },
+    error: function(data){
+      console.log('chatterbox: Failed to get messages', data);
+    }
+  });
+};
+
+
+var postMessage = function(messageString){
+  //construct the motha fucka 
+  var toTransmit = JSON.strigify({
+    'userName': this.userName,
+    'text': messageString,
+    'roomName': 'lobby' //change this!
+  });
+
+   $.ajax({
+      url: urlz,
+      type: 'POST',
+      data: toTransmit,
+      success: function(data){
+        retrieveMessages();
+      },
+      error: function(data){
+        console.log('chatterbox: Failed to send message', data);
+      }
+   });
+};
 // retrievemessages() returns messages
 
 // refresh messages
