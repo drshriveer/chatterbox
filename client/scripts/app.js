002e20fd5ceb99a598af5b2047475096fd0f5983
Hack Reactor Students <github@hackreactor.com>
2013-10-07 16:04:32 -0700

    outlines methods to code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,14 @@
 // YOUR CODE HERE:
+
+// displayMessage(messages)
+
+// retrievemessages() returns messages
+
+// refresh messages
+    // uses setTimeout(function(retrieveMessages()) {}, 1000);
+
+//postMessage(text)
+    // strigify json object with userName, roomName, text
+    // called when user pushes 'send' button.
+
+//onButtonClick() for getting new messages
